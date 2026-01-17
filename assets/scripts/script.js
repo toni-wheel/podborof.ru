09988a68686174cf16fe7e4677ca910d82b07b52
anton <toni-wheel@inbox.ru>
2026-01-17 17:47:49 +0500

    Продолжаю работу

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -68,32 +68,6 @@ lightbox.option({
   wrapAround: true,
 });
 
-ymaps.ready(function () {
-  var myMap = new ymaps.Map("yandex-map", {
-    center: [56.838, 60.598],
-    zoom: 14,
-    controls: ["zoomControl", "fullscreenControl"],
-  });
-
-  var myPlacemark = new ymaps.Placemark(
-    [56.838, 60.598],
-    {
-      balloonContentHeader: "ул. Хохрякова, 61, Екатеринбург",
-      balloonContentBody:
-        '<p><strong>Телефон:</strong> <a href="tel:+79326109113">+7 932 610 91 13</a></p>' +
-        '<p><strong>Почта:</strong> <a href="mailto:cck96@yandex.ru">Cck96@yandex.ru</a></p>' +
-        "<p><strong>Часы работы:</strong> Пн-Вс: 10:00 - 21:00</p>",
-      balloonContentFooter: "Контактная информация",
-    },
-    {
-      preset: "islands#redDotIcon",
-    }
-  );
-
-  myMap.geoObjects.add(myPlacemark);
-  myPlacemark.balloon.open();
-});
-
 document.addEventListener("DOMContentLoaded", function () {
   const toggleBtn = document.querySelector(".about-us__toggle");
   const moreText = document.querySelector(".about-us__more");
